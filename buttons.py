9263708ee689323a182ed850cb522c0135d680e9
Nat Birrer <natbirrer@gmail.com>
2019-01-11 22:14:05 -0500

    Added hardware volume control

--- buttons.py
+++ buttons.py
@@ -6,10 +6,15 @@ import RPi.GPIO as GPIO
 import os
 import signal
 import sys
+import alsaaudio as audio
 
 channel_list = [17, 22, 23, 27]
 backlightOn = True
 scanRunning = False
+
+# Setup audio control
+mixer = audio.Mixer('PCM')
+vol = mixer.getvolume()[0]
 
 def toggleBacklight(channel):
     global backlightOn
@@ -27,11 +32,6 @@ def buttonEvent(channel):
     print "Button #%d pressed for %f seconds." % (channel, time.time() - startTime)
 
 def runMorrisMain(channel):
-    #startTime = time.time()
-    #while GPIO.input(channel) == GPIO.LOW:
-    #    time.sleep(0.02)
-    ##os.system("bash /home/pi/runScanner.sh")
-    #subprocess.Popen("/home/pi/runScanner.sh", shell=True, stdout=subprocess.PIPE)
     global scanRunning
     global scan
     if scanRunning:
@@ -57,26 +57,42 @@ def poweroff(channel):
         backlightOn = True
         backlight.start(100)
 
-def graceful_exit(signum, frame):
+def volumeUp(channel):
+    global vol
+    global mixer
+    if vol < 100:
+        vol += 5 # TODO: volume maps nonlinearly
+        mixer.setvolume(vol)
+        vol = mixer.getvolume()[0]
+
+def volumeDown(channel):
+    global vol
+    global mixer
+    if vol > 0:
+        vol -= 5 # TODO: volume maps nonlinearly
+        mixer.setvolume(vol)
+        vol = mixer.getvolume()[0]
+
+def gracefulExit(signum, frame):
     print "Got an exit signal"
     backlight.stop()
     GPIO.cleanup()
     sys.exit()
 
 # Catch termination signals
-signal.signal(signal.SIGINT, graceful_exit)
-signal.signal(signal.SIGTERM, graceful_exit)
+signal.signal(signal.SIGINT, gracefulExit)
+signal.signal(signal.SIGTERM, gracefulExit)
 
-# Setup
+# Setup GPIO
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(channel_list, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(18, GPIO.OUT)
 backlight = GPIO.PWM(18, 1000)
 backlight.start(100)
 
-# TODO Maybe make buttons 17 and 22 hardware volume control
-#GPIO.add_event_detect(17, GPIO.FALLING, callback=toggleBacklight, bouncetime=200)
-GPIO.add_event_detect(22, GPIO.FALLING, callback=toggleBacklight, bouncetime=200)
+# TODO Make volume control buttons linear, not log scale!
+GPIO.add_event_detect(17, GPIO.FALLING, callback=volumeUp, bouncetime=200)
+GPIO.add_event_detect(22, GPIO.FALLING, callback=volumeDown, bouncetime=200)
 GPIO.add_event_detect(23, GPIO.FALLING, callback=runMorrisMain, bouncetime=200)
 GPIO.add_event_detect(27, GPIO.FALLING, callback=poweroff, bouncetime=200)
 
